9511db0257f60dccd033b10ebf8d757edef0c334
Kihara, Takuya <gray@tacck.net>
2021-03-26 14:56:59 +0900

    add amplify ui packages

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import "@aws-amplify/ui-vue";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
 
 Vue.config.productionTip = false;
 
